a46c25ae512b5e5c776c65ac618592f6881ce00c
Max Ye <maxye@berkeley.edu>
2022-10-05 19:04:07 -0700

    default app

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
+function MyApp() {
+  return (
+    <ChakraProvider>
+      <h1>Hello</h1>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
