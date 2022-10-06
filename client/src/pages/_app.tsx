181473b23dd0edac6159797430fd67976d5957a0
Max Ye <maxye@berkeley.edu>
2022-10-05 21:20:17 -0700

    test

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import NavBar from '../components/NavBar';
 
 function MyApp() {
   return (
     <ChakraProvider>
       <h1>Hello</h1>
+      <NavBar />
     </ChakraProvider>
   );
 }
