c8e3f4d5adf41ba1beeb8e31768c7154890141f0
Max Ye <maxye@berkeley.edu>
2022-10-12 19:19:23 -0700

    add branch

--- client/src/components/documents/index.tsx
+++ client/src/components/documents/index.tsx
@@ -5,47 +5,43 @@ const Documents = () => {
     <Grid templateColumns="repeat(6, 1fr)" mx={50} my={10} gap={8}>
       <GridItem>
         <Link href="/"></Link>
-        <Image src="BlueDocument.svg" alt="Notiom logo"/>
-
+        <Image src="BlueDocument.svg" alt="Notiom logo" />
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
-      </GridItem>
-
-
-      <GridItem>
-      <Button variant="document"> 
-      <Text> Lorem ipsum dolor sit amet, consectetur </Text>
-       </Button>
-
-
+        <Button variant="document"> STUFF </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document">
+          <Text> Lorem ipsum dolor sit amet, consectetur </Text>
+        </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
-      </GridItem>      <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document"> STUFF </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
-      </GridItem>      <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document"> STUFF </Button>
+      </GridItem>{' '}
+      <GridItem>
+        <Button variant="document"> STUFF </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
-      </GridItem>      <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document"> STUFF </Button>
+      </GridItem>{' '}
+      <GridItem>
+        <Button variant="document"> STUFF </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document"> STUFF </Button>
+      </GridItem>{' '}
+      <GridItem>
+        <Button variant="document"> STUFF </Button>
       </GridItem>
       <GridItem>
-      <Button variant="document"> STUFF </Button>
+        <Button variant="document"> STUFF </Button>
       </GridItem>
-
-      
+      <GridItem>
+        <Button variant="document"> STUFF </Button>
+      </GridItem>
     </Grid>
   );
 };
